de03895bd73ed9ef8b08057b0bf5e90d4e2c3f3f
Nathaniel Thomas <nathaniel@swbell.net>
2024-12-06 01:09:02 -0600

    removed utilsnips, was complaining about no python3 provider

--- lua/after/plugin/ultisnips.lua
+++
@@ -1,7 +0,0 @@
--- Trigger configuration
-vim.g.UltiSnipsExpandTrigger = "<Tab>"
-vim.g.UltiSnipsJumpForwardTrigger = "<Tab>"
-vim.g.UltiSnipsJumpBackwardTrigger = "<S-Tab>"
-
--- If you want :UltiSnipsEdit to split your window
-vim.g.UltiSnipsEditSplit = "vertical"
