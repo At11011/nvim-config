267d8cfd84017639d298bedefbd434e22d1c5f1f
Nathaniel Thomas <nathaniel@swbell.net>
2025-01-13 09:01:46 -0600

    updated for formatting

--- lua/after/plugin/colors.lua
+++ lua/after/plugin/colors.lua
@@ -4,6 +4,8 @@ function ColorMyPencils(color)
 
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalFloatNC", { bg = "none" })
 end
 
 ColorMyPencils()
